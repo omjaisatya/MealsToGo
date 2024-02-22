7d849d8448f53888624e8357838b79112d611485
Satya Prakash Kumar <satyaprakash451256@gmail.com>
2024-02-22 19:36:38 +0530

    started react native projects

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { Text } from "../../../components/typography/text.component";
+
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos = [
+      "https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily = true,
+    placeId,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text varient="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map((_, i) => (
+              <SvgXml
+                key={`star-${placeId}-${i}`}
+                xml={star}
+                width={20}
+                height={20}
+              />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="error">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Icon source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
---
+++ src/services/location/location.context.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+import { locationRequest, locationTransform } from "./location.service";
+
+export const LocationContext = React.createContext();
+
+export const LocationContextProvider = ({ children }) => {
+  const [keyword, setKeyword] = useState("San Francisco");
+  const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const onSearch = (searchKeyword) => {
+    setIsLoading(true);
+    setKeyword(searchKeyword);
+    if (!searchKeyword.length) {
+      // dont do anything
+      return;
+    }
+    locationRequest(searchKeyword.toLowerCase())
+      .then(locationTransform)
+      .then((result) => {
+        setIsLoading(false);
+        setLocation(result);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError(error);
+        console.log(error);
+      });
+  };
+  return (
+    <LocationContext.Provider
+      value={{ isLoading, error, location, search: onSearch, keyword }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
